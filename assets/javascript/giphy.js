8e43152ab5dbd4baa883682bb6e885ff3dd5fe17
lookin.jazmin@outlook.com <lookin.jazmin@outlook.com>
2018-11-10 22:22:14 -0800

    fedup

---
+++ assets/javascript/giphy.js
@@ -0,0 +1,58 @@
+
+$(document).ready(function() {
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+      })
+
+      
+    
+        .then(function (response) {
+        
+          var results = response.data;
+
+         
+          for (var i = 0; i < results.length; i++) {
+
+            
+            if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
+              
+              var gifDiv = $("<div>");
+
+             
+              var rating = results[i].rating;
+
+              
+              var p = $("<p>").text("Rating: " + rating);
+
+
+              var personImage = $("<img>");
+
+              
+              personImage.attr("src", results[i].images.fixed_height.url);
+            
+              gifDiv.append(p);
+              gifDiv.append(personImage);
+
+              
+              $("#gifs-appear-here").prepend(gifDiv);
+            }
+          }
+
+          $(document).ready(function() {
+            var icon = $('.random dancing');
+            icon.click(function() {
+               icon.toggleClass('active');
+               return false;
+            });
+          });
+
+    var renderButtons = function() {
+        for (i = 0; i < topics.length; i++) {
+            $("#buttonville").append(`<button class="btn btn-sm btn-dark giphy-button" data-subject="${topics[i]}">${topics[i]}</button>`)
+        }
+    }
+  
+
+        });
+    });
